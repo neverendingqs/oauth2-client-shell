e7985188165c99c9d156bb8d92bfdb7f8d33221f
Mark Tse <mark.tse@neverendingqs.com>
2016-07-02 13:01:18 -0400

    Authorization code is now based on a variable.

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('index');
+    var locals = {
+        auth_code: req.query.code
+    };
+
+    res.render('index', locals);
+});
+
+app.get('/auth', function(req, res) {
+    var authCodeRequest = req.query.auth_endpoint
+        + "?response_type=code"
+        + "&client_id=" + req.query.client_id
+        + "&redirect_uri=" + req.query.redirect_uri
+        + "&scope=" + req.query.scope;
+
+    res.redirect(authCodeRequest);
 });
 
 app.listen(port);
