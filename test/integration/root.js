66333d76ab569d778229fb8b675fb1364f05360a
Mark Tse <neverendingqs@users.noreply.github.com>
2016-11-12 21:37:49 -0500

    ci - adding cookie to smoke test and adding new test for reset. (#41)

--- test/integration/root.js
+++ test/integration/root.js
@@ -7,6 +7,23 @@ describe('GET /', function() {
     it('initial load', function(done) {
         request(app)
             .get('/')
-            .expect(200, done);
+            .expect(200)
+            .expect('set-cookie', /_csrf=.*?; Path=\//, done);
+    });
+
+    it('reset parameter resets cookie', function(done) {
+        var preExistingCookie = createCookieString({foo: "bar"});
+        var responseCookie = new RegExp(createCookieString() + "; Path=/");
+
+        request(app)
+            .get('/?reset=true')
+            .set('Cookie', preExistingCookie)
+            .expect(200)
+            .expect('set-cookie', responseCookie, done);
     });
 });
+
+function createCookieString(cookie) {
+    if(cookie) { return "oAuth2ClientShell=" + JSON.stringify(cookie); }
+    return "oAuth2ClientShell=";
+}
