9ed56831df4cee747d8a2f0a017786a4f5a8d536
Mark Tse <neverendingqs@users.noreply.github.com>
2016-11-15 22:04:56 -0500

    test - adding more assertions to /auth tests. (#51)

--- test/integration/routes/auth.js
+++ test/integration/routes/auth.js
@@ -6,7 +6,7 @@ const app = require('../../../src/server');
 const getCsrfTokens = require('../../utils').getCsrfTokens;
 
 describe('POST /auth', function() {
-    it('happy path', function(done) {
+    it('redirects user with proper params', function(done) {
         getCsrfTokens(function(cookieCsrf, formCsrf) {
             request(app)
                 .post('/auth')
@@ -14,14 +14,23 @@ describe('POST /auth', function() {
                 .type('form')
                 .send({
                     _csrf: formCsrf,
-                    auth_endpoint: 'auth_endpoint',
+                    auth_endpoint: 'https://auth_service.com/authorization_endpoint',
                     client_id: 'client_id',
                     scope: 'scope',
                     custom_params: 'key1=value1&key2=value2',
                     state: 'state'
                 })
-                // WIP
-                .expect(302, done);
+                .expect(302)
+                .end(function(err, res) {
+                    assert.include(res.header.location, 'https://auth_service.com/authorization_endpoint');
+                    assert.include(res.header.location, 'response_type=code');
+                    assert.include(res.header.location, 'redirect_uri=');
+                    assert.include(res.header.location, 'client_id=client_id');
+                    assert.include(res.header.location, 'scope=scope');
+                    assert.include(res.header.location, 'state=state');
+                    assert.include(res.header.location, 'key1=value1&key2=value2');
+                    done();
+                });
         });
     });
 });
