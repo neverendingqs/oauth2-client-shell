b2c6edb3f433ca58ec54bd10a936887063ac0682
Mark Tse <neverendingqs@users.noreply.github.com>
2016-11-16 17:59:26 -0500

    test - refactoring utils file. (#52)

--- test/utils.js
+++ test/utils.js
@@ -2,26 +2,30 @@ const request = require('supertest');
 
 const app = require('../src/server');
 
-module.exports = {
-    createCookieString: function(cookie) {
-        if(cookie) {
-            return "oAuth2ClientShell="
-                // https://github.com/expressjs/express/issues/2815
-                + encodeURIComponent("j:" + JSON.stringify(cookie));
-            }
-        return "oAuth2ClientShell=";
-    },
-    getCsrfTokens: function(cb) {
-        request(app)
-            .get('/')
-            .expect('set-cookie', /_csrf=.*?[;,]/)
-            .expect(/name="_csrf" value="(.*?)"/)
-            .end(function(err, res) {
-                if (err) return done(err);
+function createCookieString(cookie) {
+    if(cookie) {
+        return "oAuth2ClientShell="
+            // https://github.com/expressjs/express/issues/2815
+            + encodeURIComponent("j:" + JSON.stringify(cookie));
+        }
+    return "oAuth2ClientShell=";
+}
 
-                const cookieCsrf = /_csrf=(.*?);/.exec(res.headers['set-cookie'])[1];
-                const formCsrf = /name="_csrf" value="(.*?)"/.exec(res.text)[1];
-                cb(cookieCsrf, formCsrf);
-        });
-    }
+function getCsrfTokens(cb) {
+    request(app)
+        .get('/')
+        .expect('set-cookie', /_csrf=.*?[;,]/)
+        .expect(/name="_csrf" value="(.*?)"/)
+        .end(function(err, res) {
+            if (err) return done(err);
+
+            const cookieCsrf = /_csrf=(.*?);/.exec(res.headers['set-cookie'])[1];
+            const formCsrf = /name="_csrf" value="(.*?)"/.exec(res.text)[1];
+            cb(cookieCsrf, formCsrf);
+    });
+}
+
+module.exports = {
+    createCookieString: createCookieString,
+    getCsrfTokens: getCsrfTokens
 };
